4a30fdce0ad0658fa75fae13df8405f7212b7e17
Alex <alexcigun@gmail.com>
2016-07-16 22:22:47 +0300

    Все в одном файле пока

---
+++ A-gun/A gun initial steps.py
@@ -0,0 +1,78 @@
+from tkinter import *
+from random import choice, randint as rnd
+
+import time
+from math import sin, cos, pi
+
+class Ball:
+    def __init__(self,canvas,r,color,x=100,y=300):
+        self.canvas = canvas
+        self.x=x
+        self.y=y
+        self.r=r
+        self.color=color
+        self.id = canvas.create_oval(self.x-r,self.y-r,self.x+r,self.y+r, fill=self.color)
+
+class Target:
+     def __init__(self,canvas):
+        self.canvas = canvas
+        self.x = rnd(700,1000-100)
+        self.y = rnd(100,500)
+        self.r = rnd(10,50)
+        self.id = canvas.create_oval(self.x-self.r,self.y-self.r,self.x+self.r,self.y+self.r, fill='grey')
+                                     
+               
+
+
+def snaryad(tetta=45,v0=100):
+    # задаёмся переменными
+    x0=50    # координата х
+    # tetta=60 # начальный угол 
+    g=9.81   # ускорение свободного падения (м/с**2), можно подставить значение и для Луны
+    # v0=100   # начальная скорость (м/с), типовая скорость снаряда
+    y0=400   # начальная вертикальная координата 
+ 
+    # сразу задаёмся радианами
+    rad=180/pi
+    tetta_r=tetta/rad # вместо градусов получили количество радиан     
+    a=Ball(canvas,30,'red',x0,y0)
+    t=0
+    x=50
+    while x<1000+a.r:
+        v0x=v0*cos(tetta_r)
+        v0y=v0*sin(tetta_r)
+        sx=v0x*t
+        sy=-v0y*t+(g*t**2)/2
+        x=x0+sx
+        y=y0+sy
+        obj=a.id
+        try:
+            canvas.delete(obj)
+            col='red'
+            a = Ball(canvas,30,col,x,y)
+            tk.update_idletasks()
+            tk.update()
+        except:
+            pass
+        t+=0.5
+        time.sleep(0.3)
+
+    
+        
+
+if __name__=='__main__':
+    tk = Tk()
+    tk.title("Game")
+    tk.resizable(0, 0)
+    tk.wm_attributes("-topmost", 1)
+    canvas = Canvas(tk, width=1000, height=600, bd=0, highlightthickness=0)
+    canvas.pack()
+    targets_count=rnd(2,6)
+    targets=[]
+    for i in range(targets_count):
+        t=Target(canvas)
+        targets.append(t)
+    tk.update()                                     
+    v0=100   # начальная скорость (м/с), типовая скорость снаряда
+    tetta=60 # начальный угол
+    snaryad(tetta,v0)
